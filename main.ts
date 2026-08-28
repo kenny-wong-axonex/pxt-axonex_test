5eee11e568bb09b705543bf3e1228abea7c6b963
kenny-wong-axonex <kenny.wong@axonex.ai>
2026-08-28 15:16:33 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
-})
+namespace R300 {
+    //% block
+    export function test() {
+        return "test";
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
+    }
+}
