cb87aa92bfcb219985132412253d390acfece9c4
kenny-wong-axonex <kenny.wong@axonex.ai>
2026-09-09 13:58:03 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -18,7 +18,7 @@ namespace r300 {
             if (!this.listenerRegistered) {
                 this.listenerRegistered = true;
                 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), () => {
-                    const line = serial.readLine().trim();
+                    const line = serial.readUntil(serial.delimiters(Delimiters.NewLine)).trim();
                     if (line.includes("_ack")) {
                         this.latestAck = line;
                     } else if (line.includes("_finish")) {
